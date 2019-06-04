13550c19aaa73f6a3f13ea8439abf1ef24f1cf1c
Azhar Asif <azhar.asif94@gmail.com>
2019-06-04 23:30:00 +0500

    Uploading working version

--- src/android/FirebaseStoragePlugin.java
+++ src/android/FirebaseStoragePlugin.java
@@ -1,59 +1,58 @@
 package by.azharjaliawala.cordova.firebase;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
+import by.chemerisuk.cordova.support.CordovaMethod;
+import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
+
+import android.util.Log;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnPausedListener;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageException;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.google.firebase.FirebaseException;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.PluginResult;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class FirebaseStoragePlugin extends CordovaPlugin {
+public class FirebaseStoragePlugin extends ReflectiveCordovaPlugin {
 
+    private static final String TAG = "FirebaseStorage";
+
+    private FirebaseStorage storage;
+
+    @Override
+    protected void pluginInitialize() {
+        Log.d(TAG, "Starting Firebase Storage plugin");
+
+        this.storage = FirebaseStorage.getInstance();        
+    }
+
+    @CordovaMethod
     public void uploadPicture(String picUrl, String picName, final CallbackContext callbackContext) {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-
-        // Create a storage reference from our app
         StorageReference storageRef = storage.getReference();
 
-        Uri file = Uri.fromFile(new File(picUrl));
+        Uri file = Uri.parse(picUrl);
         StorageMetadata metadata = new StorageMetadata.Builder()
                 .setContentType("image/jpeg")
                 .build();
-        StorageReference imageRef = storageRef.child("images/"+file.getLastPathSegment());
+        StorageReference imageRef = storageRef.child("images/"+picName);
         UploadTask uploadTask = imageRef.putFile(file, metadata);
 
         uploadTask.addOnSuccessListener(cordova.getActivity(), new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 // taskSnapshot.getMetadata() contains file metadata
-                callbackContext.success();
+                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        callbackContext.success(uri.toString());
+                    }
+                });
             }
         }).addOnFailureListener(cordova.getActivity(), new OnFailureListener() {
             @Override
